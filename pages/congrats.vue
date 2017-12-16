50467831a39710a493adaaa247b087ac13ab350c
Carlos Rodrigo <carlos.rodrigo@mercadolibre.com>
2017-12-16 20:22:51 -0300

    congrats makeup

--- pages/congrats.vue
+++ pages/congrats.vue
@@ -1,9 +1,18 @@
 <template>
-  <div>
-    <p> Felicitaciones disfruta clarovideo gracias a MercadoPuntos.</p>
-  </div>
-
-
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+     <div class="text-xs-center">
+        <img src="../assets/img/JimmyStreamingLogo.png" alt="Vuetify.js" class="mb-5" />
+      </div>
+      <v-card class="transparent-background">
+       <v-card-title class="headline">Felicitaciones! Ya podes disfrutar de todo el contenido de Jimmy Streaming :)</v-card-title>
+       <v-card-actions>
+          <v-spacer></v-spacer>
+          <a href="#">Acciones</a>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
